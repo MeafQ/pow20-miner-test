75c8c0d003b67662474f4df8954fa8fd729fe081
Vladyslav Ohlii <53783400+malatindez@users.noreply.github.com>
2024-03-07 16:42:50 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ pub async fn submit_work(solution: &Solution, ctx: &Context) -> () {
 async fn main() -> Result<()> {
     let args = Args::parse();
     let num_threads = num_cpus::get();
-    
+
     rayon::ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
 
 
@@ -154,7 +154,7 @@ async fn main() -> Result<()> {
 
     let mut nonce: u16 = 1;
     let bucket = (0..16_000_000).collect::<Vec<u32>>();
-    let chunk_size: u32 = 100_000;
+    let chunk_size: usize = 100_000;
     loop {
         let start_time = Instant::now();
 
