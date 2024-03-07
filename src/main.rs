bda5638fadeb45d4cf92cfd2475d8f5d7c0e5898
Vladyslav Ohlii <53783400+malatindez@users.noreply.github.com>
2024-03-07 16:45:19 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -153,7 +153,8 @@ async fn main() -> Result<()> {
     });
 
     let mut nonce: u16 = 1;
-    let bucket = (0..(num_cpus*1_000_000)).collect::<Vec<u32>>();
+    let mut bucket_size:usize = num_cpus*1_000_000;
+    let bucket = (0..bucket_size).collect::<Vec<u32>>();
     let chunk_size: usize = 1_000_000;
     loop {
         let start_time = Instant::now();
